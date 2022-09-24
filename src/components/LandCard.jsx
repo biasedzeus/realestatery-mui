5dfe4422e2866dc70773b8b776585c7b543c2dd9
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-09-24 15:14:09 +0530

    card done

--- src/components/LandCard.jsx
+++ src/components/LandCard.jsx
@@ -1,39 +1,61 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea, Stack } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, Chip, Stack } from "@mui/material";
+import { BiBath, BiBed, BiArea } from "react-icons/bi";
+import styled from "@emotion/styled";
 
-export default function ActionAreaCard({product}) {
-  const {title,price,image,address:{street,city,state},bhk:{bedrooms,bathrooms,area}} = product;
+const BHKIcons = styled(Typography)({
+  display: "flex",
+  alignItems: "center",
+  gap: ".4rem",
+});
+
+export default function ActionAreaCard({ product }) {
+  const {
+    title,
+    price,
+    image,
+    address: { street, city, state },
+    bhk: { rooms, bathrooms, area },
+  } = product;
   return (
-    <Card sx={{ minWidth:300,maxWidth: 345 }}>
+    <Card sx={{ minWidth: 300, maxWidth: 345 }}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={image}
-          alt="green iguana"
-        />
+        <CardMedia component="img" height="140" image={image} alt={title} />
+
+  
         <CardContent>
-          <Typography fontWeight="800" color="purple" variant="body2" >
-            ${price} <span className='month'>/ month</span>
+          <Typography fontWeight="800" color="purple" variant="body2">
+            ${price} <span className="month">/ month</span>
           </Typography>
           <Typography gutterBottom variant="h6" component="div">
             {title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-             {street + city + state} 
+            {street + city + state}
           </Typography>
-          <hr/>
-          <Stack>
-            <Typography></Typography>
-            <Typography></Typography>
-            <Typography></Typography>
-
+          <hr />
+          <Stack
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-evenly"
+          >
+            <BHKIcons>
+              <BiBed />
+              {rooms}
+            </BHKIcons>
+            <BHKIcons>
+              <BiBath />
+              {bathrooms}
+            </BHKIcons>
+            <BHKIcons>
+              <BiArea />
+              {area} sq. m
+            </BHKIcons>
           </Stack>
-         
         </CardContent>
       </CardActionArea>
     </Card>
