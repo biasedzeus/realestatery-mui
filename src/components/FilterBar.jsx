f9a94ccde0240ec8c71408f2e8920e56f302ee17
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-09-25 10:12:23 +0530

    price slider added propType fixed

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -4,11 +4,17 @@ import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import styled from "@emotion/styled";
-import { Box, Button, Popover, Portal, Stack, Typography,TextField } from "@mui/material";
+import {
+  Button,
+  Popover,
+  Stack,
+  Typography,
+  TextField,
+} from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import PriceSlider from "./Slider";
-import {MdClearAll} from 'react-icons/md';
+import { MdClearAll } from "react-icons/md";
 
 const FilterContainer = styled(Stack)({
   flexDirection: "row",
@@ -23,6 +29,11 @@ const buttonStyles = {
   backgroundColor: "purple",
 };
 
+
+
+
+
+
 const FilterBar = ({
   locations,
   filters,
@@ -31,10 +42,10 @@ const FilterBar = ({
   propertyTypeList,
   clearFilters,
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [filtersApplied, setFiltersApplied] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
 
-
+ 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -42,10 +53,20 @@ const FilterBar = ({
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const handlePriceChange = (e, newValue) =>{
+    setFilters({...filters,price:newValue})
+
+  }
   console.log(filters.moveInDate.$d);
+  console.log("property", filters);
   return (
     <FilterContainer>
-      <Stack flexDirection="row" alignItems="center" justifyContent="space-around">
+      <Stack
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-around"
+      >
         <FormControl sx={{ m: 1, minWidth: 120 }}>
           <FormHelperText>Location</FormHelperText>
           <Select
@@ -57,7 +78,6 @@ const FilterBar = ({
               setFilters({ ...filters, location: e.target.value })
             }
           >
-        
             {locations.map((location, index) => {
               return (
                 <MenuItem key={index} value={location}>
@@ -90,46 +110,57 @@ const FilterBar = ({
             sx={{
               border: "1px solid gray",
               borderRadius: "5px",
-              padding: "0 1rem",
+              padding: "1rem 2rem",
               backgroundColor: "white",
             }}
           >
-            <Typography fontWeight="700">$500 - $2000</Typography>
-
+            <Typography fontWeight="700">{`$${filters.price[0]}-$${filters.price[1]}`}</Typography>
           </Stack>
           <Popover open={anchorEl} anchorEl={anchorEl} onClose={handleClose}>
-            <PriceSlider />
+            <PriceSlider
+              price={filters.price}
+              handleChange={handlePriceChange}
+              />
           </Popover>
         </FormControl>
         <FormControl sx={{ m: 1, minWidth: 120 }}>
           <FormHelperText>Property Type</FormHelperText>
           <Select
-
             labelId="propertyType"
             id="propertyType"
-            value={filters.propertyType}
+            value={filters.propertyType || ""}
             label="propertyType"
-            onChange={(e) =>
-              setFilters({ ...filters, propertyType: e.target.value })
+            onChange={(newValue) =>
+              setFilters({ ...filters, propertyType: newValue.target.value })
             }
-          > 
-           <MenuItem value="" >select</MenuItem>
-            
-            {propertyTypeList.map((property, index) => {
-              return (
-                <MenuItem key={index} value={property}>
-                  {property}
-                </MenuItem>
-              );
-            })}
+          >
+            <MenuItem value="">select</MenuItem>
+
+            {propertyTypeList &&
+              propertyTypeList.map((property, index) => {
+                return (
+                  <MenuItem key={index} value={property}>
+                    {property}
+                  </MenuItem>
+                );
+              })}
           </Select>
         </FormControl>
       </Stack>
 
-      <Button variant="contained" sx={buttonStyles} onClick={applyFilters}>
+      <Button 
+         variant="contained"
+         sx={buttonStyles} 
+         onClick={applyFilters}>
         Search
       </Button>
-      {filters && <Button variant="outline" onClick={clearFilters}><MdClearAll/>Clear Filters</Button>}
+        <Button 
+          variant="outline"
+          onClick={clearFilters}>
+          <MdClearAll />
+          Clear Filters
+        </Button>
+     
     </FilterContainer>
   );
 };
